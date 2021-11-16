48eb50a55710f31feef0f5c42e844d7f157cb53b
fajarslvn <jarphish@gmail.com>
2021-11-16 19:58:07 +0700

    init

--- main.go
+++ main.go
@@ -1,4 +1,22 @@
 package main
 
+import (
+	"log"
+	"os"
+
+	"github.com/pkg/errors"
+)
+
 func main() {
+	if err := run(); err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+}
+
+func run() error {
+	if 1 == 2 {
+		return errors.New("random error")
+	}
+	return nil
 }
